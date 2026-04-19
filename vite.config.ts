83c2663f41dfc49dfa2f93bd1554b646aebb7134
FrndID <143871367+FrndID@users.noreply.github.com>
2026-04-19 17:56:24 +0700

    Update base path for Vite configuration

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,6 @@
 export default {
     root: './src',
-    base: process.env.NETLIFY ? '/' : '/Fantasy-Map-Generator/',
+    base: '/',
     build: {
         outDir: '../dist',
         assetsDir: './',
